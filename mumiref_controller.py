676b87b807e6495f37acfa851c7f5f29fd8eed77
pox17 <paos9904@gmail.com>
2023-11-29 23:50:27 +0100

    updated controller not working

--- mumiref_controller.py
+++ mumiref_controller.py
@@ -1,3 +1,5 @@
+import time
+import threading
 import tkinter.messagebox
 import customtkinter
 import yaml
@@ -5,6 +7,18 @@ from typing import Callable
 from pythonosc import udp_client
 from pythonosc import osc_bundle_builder
 from pythonosc import osc_message_builder
+from pythonosc import osc_server
+from pythonosc import dispatcher
+import argparse
+import numpy as np
+import pythonosc.dispatcher
+import pythonosc.osc_server
+import asyncio
+from pythonosc.osc_server import AsyncIOOSCUDPServer
+from pythonosc.dispatcher import Dispatcher
+import asyncio
+
+
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
@@ -50,6 +64,18 @@ class BinResColumn(customtkinter.CTkFrame):
         except ValueError:
             return
 
+    def print_level_db_st(self,level_db_L,level_db_R):
+        level_to_print_L = np.interp(level_db_L,[-200,+10],[0,1])
+        level_to_print_R = np.interp(level_db_R,[-200,+10],[0,1])
+        self.level_meter_L.set(level_to_print_L)
+        self.level_meter_R.set(level_to_print_R)
+
+    def handle_osc_level(self,unused_addr, function_params,  msg_args):
+        level_db_L= msg_args[0]
+        level_db_R= msg_args[1]
+        print(level_db_L)
+        self.print_level_db_st(level_db_L=level_db_L,level_db_R=level_db_R)
+
 class App(customtkinter.CTk):
     def __init__(self,bin_renderers,my_client):
         super().__init__()
@@ -69,6 +95,10 @@ class App(customtkinter.CTk):
         
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="MuMiReF", font=customtkinter.CTkFont(size=20, weight="bold"))
         self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.check_button = customtkinter.CTkButton(self, fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"),text="check",command=self.start_action)
+        self.check_button.grid(row= 1, column = 0 )
+        self.check_label = customtkinter.CTkLabel(self.sidebar_frame, text="MuMiReF", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.check_label.grid(row=2, column=0, padx=20, pady=(20, 10))
         self.clients_frame = customtkinter.CTkScrollableFrame(self, fg_color="transparent",orientation="horizontal")
         self.clients_frame.grid(row=0, column=1, padx=(0, 0), pady=(10, 10), sticky="nsew")
         self.clients_frame.grid_rowconfigure(1, weight=1)
@@ -79,6 +109,50 @@ class App(customtkinter.CTk):
             self.bin_ren_cols.append(bin_ren_col)
     def sidebar_button_event(self):
         print("sidebar_button click")
+
+    def get_bin_res_col(self,index):
+        print("working")
+        return self.bin_ren_cols[index]
+
+    def start_action(self):
+        self.check_button.configure(state="disabled")
+        thread = threading.Thread(target=self.init_main)
+        thread.start()
+        self.check_thread(thread)
+
+    # We make the button active only after the flow is completed.
+    def check_thread(self, thread):
+        if thread.is_alive():
+            self.check = self.after(500, lambda: self.check_thread(thread))
+        else:
+            self.check_button.configure(state="normal")
+
+    def init_main(self):
+        # start dispatcher,server
+        self.loop()  # Enter main loop of program
+        # Clean up serve endpoint
+
+    def loop(self):
+        """Example main loop that only runs for 10 iterations before finishing"""
+        for i in range(2):
+            print(f"Loop {i}")
+            self.check_label.configure(text=f"Loop {i}")
+            time.sleep(1)
+
+    def setup_osc_server(self):
+        inport = 5007
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--ip", default="127.0.0.1",
+            help="The ip of the OSC server")
+        parser.add_argument("--port", type=int, default=inport,
+            help="The port the OSC server is listening on")
+        args = parser.parse_args()
+        #dispatcher = dispatcher.Dispatcher()
+        dispatcher = pythonosc.dispatcher.Dispatcher()
+
+
+
 
 if __name__ == "__main__":
     with open('./config_spatial_mic_renderer_1_6.yml', 'r') as file:
@@ -103,5 +177,12 @@ if __name__ == "__main__":
     ip = "127.0.0.1"
     port = 5100
     my_client = udp_client.SimpleUDPClient(ip,port)
-    app = App(bin_renderers,my_client)
+
+    
+
+
+
+    
+    
+    
     app.mainloop()
